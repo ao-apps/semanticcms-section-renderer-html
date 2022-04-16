0ae76a8bdea9d8e7e54f1f993f2e5c69bd49d20e
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:01:38 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/semanticcms/section/renderer/html/SectionHtmlRenderer.java
+++ src/main/java/com/semanticcms/section/renderer/html/SectionHtmlRenderer.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-section-renderer-html.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.section.renderer.html;
 
 import com.aoapps.html.any.AnyFlowContent;
